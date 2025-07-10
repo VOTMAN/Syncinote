13a6c94ab4523e1f6de5547adf49ad8c458fdd0e
Arnav <76613041+VOTMAN@users.noreply.github.com>
2025-07-10 09:25:55 +0530

    Init

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import * as fs from 'node:fs'
+import { createFileRoute, useRouter } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
+
+const filePath = 'count.txt'
+
+async function readCount() {
+  return parseInt(
+    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0')
+  )
+}
+
+const getCount = createServerFn({
+  method: 'GET',
+}).handler(() => {
+  return readCount()
+})
