6a596b481d012e296b44d30f71207faffede061d
Arnav <76613041+VOTMAN@users.noreply.github.com>
2025-08-04 14:24:18 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,10 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: { port: 3000 },
   plugins: [
     tsConfigPaths(),
     tanstackStart({target: "vercel"}),
     tailwindcss(),
   ],
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  },
-  ssr: {
-    noExternal: ['react', 'react-dom'],
-  },
-  esbuild: {
-    jsx: 'automatic',
-  },
 })
 
